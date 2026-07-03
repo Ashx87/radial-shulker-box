d08c98a874cf2682932d6175167853d7672b40dd
Ashx87 <joshuang43wq@gmail.com>
2026-07-04 00:27:24 +0800

    chore: initial commit of stock Fabric template
    Baseline before implementing the radial shulker box menu feature.

---
+++ src/client/java/io/github/ashx87/radialshulkerbox/client/RadialShulkerBoxClient.java
@@ -0,0 +1,10 @@
+package io.github.ashx87.radialshulkerbox.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class RadialShulkerBoxClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
