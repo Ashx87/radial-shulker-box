5a737d9d13f7044b266e01a673f10762cd759328
Ashx87 <joshuang43wq@gmail.com>
2026-07-04 12:49:21 +0800

    feat: register radial shulker box keybinding

--- src/client/java/io/github/ashx87/radialshulkerbox/client/RadialShulkerBoxClient.java
+++ src/client/java/io/github/ashx87/radialshulkerbox/client/RadialShulkerBoxClient.java
@@ -1,10 +1,22 @@
 package io.github.ashx87.radialshulkerbox.client;
 
+import io.github.ashx87.radialshulkerbox.RadialShulkerBox;
+
 import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.fabric.api.client.keymapping.v1.KeyMappingHelper;
+
+import net.minecraft.client.KeyMapping;
+
+import org.lwjgl.glfw.GLFW;
 
 public class RadialShulkerBoxClient implements ClientModInitializer {
+	private static KeyMapping openRadialMenuKey;
+
 	@Override
 	public void onInitializeClient() {
-		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+		KeyMapping.Category category = KeyMapping.Category.register(RadialShulkerBox.id("main"));
+		openRadialMenuKey = KeyMappingHelper.registerKeyMapping(
+			new KeyMapping("key.radial-shulker-box.open", GLFW.GLFW_KEY_G, category)
+		);
 	}
 }
